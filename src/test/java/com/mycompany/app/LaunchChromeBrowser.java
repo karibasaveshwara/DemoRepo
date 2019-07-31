6230ff5c33007194b6d0ced266210054e7640a9d
kt <karibasaveshwara39@gmail.com>
2019-07-31 23:56:21 +0530

    first git commit for demo selenium code

---
+++ src/test/java/com/mycompany/app/LaunchChromeBrowser.java
@@ -0,0 +1,23 @@
+package com.mycompany.app;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class LaunchChromeBrowser {
+
+	public static void main (String args[]) throws InterruptedException{
+		System.setProperty("webdriver.chrome.driver", "C:/Users/kt/Downloads/chromedriver.exe");
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("disable-infobars");
+		WebDriver driver=new ChromeDriver(options);
+		//WebDriver driver=new ChromeDriver();
+		System.out.println("Launched chrome browser");
+		driver.manage().window().maximize();
+		System.out.println("Maximized window");
+		driver.get("http://google.com");
+		System.out.println("Title is:- "+driver.getTitle());
+		Thread.sleep(5000);
+		driver.quit();
+	}
+}
